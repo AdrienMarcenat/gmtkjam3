f0d7f6263b18a4c92dd460f9808180ff868cebc0
AdrienMarcenat <adrien.marcenat@gmail.com>
2019-08-03 10:43:04 +0200

    remove unused code in generic player controller

--- game/Assets/Scripts/Gameplay/Player/PlayerController.cs
+++ game/Assets/Scripts/Gameplay/Player/PlayerController.cs
@@ -1,17 +1,9 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 
 [RequireComponent(typeof(MovingObject))]
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float m_JumpForce = 5;
-    [SerializeField] private bool m_AirControl = false;
-    [SerializeField] private LayerMask m_WhatIsGround;
-
     private MovingObject m_Mover;
-    private Transform m_GroundCheck;
-    private const float ms_GroundedRadius = 0.2f;
-    private bool m_Grounded;
     private bool m_ProcessInput = true;
     private Vector3 m_FacingDirection;
 
@@ -21,7 +13,6 @@ public class PlayerController : MonoBehaviour
     void Awake ()
     {
         m_Mover = GetComponent<MovingObject> ();
-        m_GroundCheck = transform.Find ("GroundCheck");
         m_FacingDirection = new Vector3 (1, 0, 0);
         this.RegisterAsListener ("Player", typeof(PlayerInputGameEvent));
     }
@@ -29,18 +20,6 @@ public class PlayerController : MonoBehaviour
     private void OnDestroy ()
     {
         this.UnregisterAsListener ("Player");
-    }
-
-    private void FixedUpdate ()
-    {
-        m_Grounded = false;
-        
-        Collider2D[] colliders = Physics2D.OverlapCircleAll (m_GroundCheck.position, ms_GroundedRadius, m_WhatIsGround);
-        foreach (Collider2D collider in colliders)
-        {
-            if (collider.gameObject != gameObject)
-                m_Grounded = true;
-        }
     }
 
     public void OnGameEvent(PlayerInputGameEvent inputEvent)
@@ -53,10 +32,10 @@ public class PlayerController : MonoBehaviour
         EInputState state = inputEvent.GetInputState ();
         switch (input)
         {
-            case "Jump":
+            case "Undo":
                 if (state == EInputState.Down)
                 {
-                    Jump ();
+                    Undo ();
                 }
                 break;
             case "Right":
@@ -72,21 +51,11 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    private void Jump()
+    private void Undo()
     {
-        if (m_Grounded)
-        {
-            // Don't wait fo the physic to set m_Grounded to false to ensure applying the force only once
-            m_Grounded = false;
-            m_Mover.ApplyForce (new Vector2 (0f, m_JumpForce));
-        }
     }
 
     private void Move (float xDir)
     {
-        if (m_Grounded || m_AirControl)
-        {
-            m_Mover.MoveHorizontal (xDir);
-        }
     }
 }
