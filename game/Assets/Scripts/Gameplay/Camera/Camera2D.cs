4a6988a9d089877f1c422d3033cd012e82786e6c
AdrienMarcenat <adrien.marcenat@gmail.com>
2019-08-03 21:50:08 +0200

    integrate some sprites, dynamically change screen resolution to match level size

--- game/Assets/Scripts/Gameplay/Camera/Camera2D.cs
+++ game/Assets/Scripts/Gameplay/Camera/Camera2D.cs
@@ -5,6 +5,7 @@ public class Camera2D : MonoBehaviour
     void Start()
     {
         TileCoordinates levelDimension = LevelManagerProxy.Get().GetLevelDimension();
-        transform.position = new Vector3(levelDimension.x.ToWorldUnit() / 2, levelDimension.y.ToWorldUnit() / 2, transform.position.z);
+        transform.position = new Vector3(-0.5f + levelDimension.x.ToWorldUnit() / 2, levelDimension.y.ToWorldUnit() / 2, transform.position.z);
+        Screen.SetResolution(48 * levelDimension.x, 3*48, false);
     }
 }
